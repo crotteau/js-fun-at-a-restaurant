54fbc1c752615c44aff5cf8437c5d8b2a9c9620e
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-02 22:23:24 -0700

    preliminary unit test code

--- src/order.js
+++ src/order.js
@@ -1,7 +1,47 @@
+// takeOrder
+var deliveryOrders = []
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length >= 3) {
+    var order =
+    deliveryOrders.push(order)
+    deliveryOrders.pop()
+    return deliveryOrders
+  } else {
+      var order =
+      deliveryOrders.push(order)
+      return deliveryOrders
+  }
+}
+
+// refundOrder
+function refundOrder(orderNumber, deliveryOrders) {
+  var refund = (element => element.orderNumber === orderNumber)
+  var index = deliveryOrders.findIndex(refund)
+    deliveryOrders.splice(index, 1)
+    return deliveryOrders
+  }
+
+// listItems
+function listItems(orders) {
+  var itemNames = [];
+  for (var i = 0; i < orders.length; i++) {
+    itemNames.push(orders[i].item);
+    finalItems = (itemNames.join(', '))
+  }
+  return finalItems;
+}
+
+// // searchOrder
+function searchOrder(deliveryOrders, foodName) { 
+ for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].item === foodName)
+    return true
+ } return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder 
 }
